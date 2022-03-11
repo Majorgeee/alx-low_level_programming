d2deb42a332c6c5fd71cc3aafae5d64d22378342
Majorgeee <iraborosas4@gmail.com>
2022-03-10 18:43:10 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/** 
-*main - entry point 
-* 
-*Return: 0 after printing the fubction 
+/**
+*main-entry point 
+*
+*Return:0 after printing the fubction 
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 } 
